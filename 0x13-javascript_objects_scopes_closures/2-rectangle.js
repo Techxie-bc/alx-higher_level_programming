c2cdab7c0c32ea0c3fecf1dd02045194332ce91d
Techxie-bc <donnaogbu@gmail.com>
2022-12-27 17:58:45 +0100

    fixed error for 2-rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
-      [this.width, this.height] = [w, h];
-    } else {
-      let obj = {};
+      this.width = w;
+      this.height = h;
     }
   }
-};
+}
+
+module.exports = Rectangle;
